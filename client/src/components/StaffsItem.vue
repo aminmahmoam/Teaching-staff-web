33293fd3477766542dbdc0203727ddf7d57eafb6
Yasamin <gusfazya@student.gu.se>
2022-10-04 15:43:15 +0200

    Add search functions for students and staffs, Add upload file function

--- client/src/components/StaffsItem.vue
+++ client/src/components/StaffsItem.vue
@@ -1,51 +1,42 @@
 <template>
    <div>
     <div class="card">
-    <div class="color">
-      <span></span>
-    </div>
     <div>
-      <span class="staffName">{{staff.firstName}} {{staff.lastName}}</span>
+      <p>{{staff.firstName}} {{staff.lastName}}</p>
       <div>
-      <span class="staffCode">{{staff.SSN}}</span>
+      </div>
+      <p>Email: {{staff.emailAddress}}</p>
+      <p>Telephone: {{staff.telephone}}</p>
     </div>
     </div>
   </div>
- </div>
   </template>
 
 <script>
 export default {
   name: 'staff-item',
   props: ['staff']
+  /*
+  computed: {
+    Staffs: function () {
+      return this.staffs.filter((staff) => {
+        return staff.firstName.match(this.search)
+      })
+    }
+  }
+  */
 }
 </script>
-  <style scoped>
+<style scoped>
   .card {
-    display: inline;
-    position: relative;
-    width: 15rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 150px;
     background: white;
     padding: 8px;
     border-radius: 10px;
-    display: flex;
     cursor: pointer;
     box-shadow: 0 7px 25px rgba(0,0,0, 0.08);
-  }
-  .color {
-    display: flex;
-    width: 14rem;
-    margin-left: 0;
-    height: 10rem;
-    margin-right: 15px;
-    background-color: rgb(152, 211, 63);
-  }
-  .button{
-  display: flex;
-  align-items: center;
-  min-width: none;
-  text-decoration: none;
-  padding: 0.5rem;
-  margin-bottom: 1rem;
   }
   </style>
